39df1dea77eedcbf34f40f9b8f5a3673c382ea3e
Metin Vural <metin.vural@hhi.fraunhofer.de>
2023-12-18 16:42:10 +0100

    up

---
+++ sendemail.php
@@ -0,0 +1,10 @@
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $to = 'your-email@example.com';
+    $subject = 'New Contact Form Submission';
+    $body = "Name: $name\nEmail: $email\nMessage: $message";
+
+    mail($to, $subject, $body);
+}
